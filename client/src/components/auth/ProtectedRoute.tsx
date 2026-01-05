01f9168b823a1a121b9081f7ee4c48d4fadcfa91
Prithivi <prithivi@users.noreply.github.com>
2026-01-05 15:38:54 +0545

    feat: implement ProtectedRoute and AppLayout components with authentication handling

--- client/src/components/auth/ProtectedRoute.tsx
+++ client/src/components/auth/ProtectedRoute.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Spinner } from "../ui/spinner";
+import { Navigate, Outlet } from "react-router-dom";
+import AppLayout from "../layout/AppLayout";
 
 const ProtectedRoute = () => {
-  return <div>ProtectedRoute</div>;
+  const isAuthenticated = false;
+  const loading = false;
+
+  if (loading) {
+    return (
+      <div className="h-scree flex-center">
+        <Spinner />
+      </div>
+    );
+  }
+  return isAuthenticated ? (
+    <AppLayout>
+      <Outlet />
+    </AppLayout>
+  ) : (
+    <Navigate to="/login" replace />
+  );
 };
 
 export default ProtectedRoute;
