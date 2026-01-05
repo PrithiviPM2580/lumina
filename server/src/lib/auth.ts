3d17269df28fb3bd5f7159c07a394252268b3698
Prithivi <prithivi@users.noreply.github.com>
2026-01-05 17:43:44 +0545

    feat: update authentication module to include social provider support and environment variables

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { getDatabaseConnection } from "@/config/database.config.js";
 import APIError from "./error.js";
+import { bearer } from "better-auth/plugins";
 
 const client = getDatabaseConnection();
 
@@ -10,9 +11,19 @@ if (!client.db) {
 }
 const auth = betterAuth({
   database: mongodbAdapter(client.db),
+  plugins: [bearer()],
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+  basePath: "/api/v1/auth",
 });
 
 export default auth;
