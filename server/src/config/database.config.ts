98a3fd19eca4093f2ba338bef316f65136557427
Prithivi <prithivi@users.noreply.github.com>
2026-01-05 16:39:17 +0545

    feat: add MongoDB connection utility and update package dependencies for Express types

---
+++ server/src/config/database.config.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export const connectToDatabse = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!);
+    console.log("Connected to MongoDB");
+  } catch (error) {}
+};
